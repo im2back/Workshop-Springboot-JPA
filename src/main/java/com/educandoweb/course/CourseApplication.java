478d5e927192bb186e23490d595c828bf419e813
Jefferson Richards <jefferson.richards@outlook.com>
2023-03-29 21:46:38 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -3,11 +3,16 @@ package com.educandoweb.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.educandoweb.course.entities.Order;
+
 @SpringBootApplication
 public class CourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
+		
+		
+
 	}
 
 }
